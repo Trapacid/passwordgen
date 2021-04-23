4146a58417b643c6623fe7aa1dc9471e2b5c56f2
Mike <zanthed@riseup.net>
2021-04-23 19:48:09 -0400

    safer stdin, seed changes, less useless vars, clean code
    Signed-off-by: Mike <zanthed@riseup.net>

--- passwordgen.c
+++ passwordgen.c
@@ -5,71 +5,66 @@
 #include <inttypes.h>
 #include <time.h>
 #include <ctype.h>
+#include <string.h>
 
-#define BILLION 1000000000L
-
-void GeneratePassword(int x, int y) {
-	//char data[36] = "1aB2cD3eF4gH5iJ6kL7mN8oP9qR0sTuVwXyZ";
-	char data[66] = "abcdefghijklmnopqrstuvwxyz-!_ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-";
+void GeneratePassword(int x, int y) 
+{
+	static const char *const CHARSET = "abcdefghijklmnopqrstuvwxyz-!_@#$ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 	char character[x];
-	character[y] = data[rand() % (sizeof(data) - 1)];
-	printf("%c", character[y]);
+	character[y] = CHARSET[rand() % (strlen(CHARSET) - 1)];
+	putchar(character[y]);
 	return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
+	char line0[256]; // if you need more than that, learn to pass two arguments lmao.
+	char line1[256];
+
 	long int nanoseconds;
-	uint64_t all;
+	uint64_t seed_1;
 	time_t seconds;
 	struct timespec spec;
 	clock_gettime(CLOCK_REALTIME, &spec);
 	seconds = spec.tv_sec;
 	nanoseconds = spec.tv_nsec;
 
-	all = (uint64_t)seconds * BILLION + (uint64_t)nanoseconds;
+	seed_1 = (uint64_t)nanoseconds * 69420 + (uint64_t)seconds;
 
-	int len;
-	uint32_t seed;
-	if(argc <= 2) {
+	uint32_t len, seed_2;
+	if (argc <= 2) {
 		printf("input seed: ");
-		int how;
-		if(scanf("%d", &how) == 1) {
-			if(how == 0) {
-				printf("enter a number that is not 0 or characters fr2\n");
-				return 1;
-			}
-			else
-				seed = how;
-		}
-		else {
-			printf("enter a number that is not 0 or characters frcoal\n");
-			return 1;
+		fgets(line0, sizeof(line0), stdin);
+		seed_2 = strtol(line0, NULL, 10);
+		if (!seed_2) {
+				printf("enter a number that is not 0 or characters\n");
+				return -1;
 		}
 
 		printf("enter length: ");
-		if(scanf("%d", &len) != 1 || len <= 3) {
-			printf("enter a number that is greater than 3 for length");
-			return 1;
+		fgets(line1, sizeof(line1), stdin);
+		len = strtol(line1, NULL, 10);
+		if (!len || len <= 3) {
+			printf("enter a number that is greater than 3 for length\n");
+			return -1;
 		}
 	}
 	else {
-		int ok = atoi(argv[1]);
+		seed_2 = atoi(argv[1]);
 		len = atoi(argv[2]);
-		if(ok == 0)
+		if (!seed_2)
 		{
 			printf("enter a number that is not 0 or letters for seed\n");
-			return 1;
+			return -1;
 		}
-		else
-			seed = ok;
-		if(len <= 3) {
-			printf("enter a number greater than 3 for length");
-			return 1;
+		if (!len || len <= 3) {
+			printf("enter a number greater than 3 for length\n");
+			return -1;
 		}
 	}
-	srand(all * seed);
-	for(int i = 0; i < len; i++) {
+	srand(seed_1 * seed_2);
+	for (int i = 0; i < len; i++)
 		GeneratePassword(len, i);
-	}
+
 	return 0;
 }
